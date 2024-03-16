b50648b839c458bfba6c7c0cd1f38f95ba4d6366
Minur Memizade <ndng.minurmemizade@gmail.com>
2024-03-16 10:45:30 -0800

    Tasks completed (｡◕ ゝ◕｡)

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿int number = 336;
+int sum = 0;
+int first = number / 100;
+int second = number % 100 / 10;
+int third = number % 10;
+if (first % 3 == 0) sum += first;
+if (second % 3 == 0) sum += second;
+if (third % 3 == 0) sum += third;
+Console.WriteLine(sum);
